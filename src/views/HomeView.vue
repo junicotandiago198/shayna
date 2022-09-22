d1b220ff30280db5b1df6426315f8a24ed3acbfc
junicotandiago198 <junicotandiagoo21@gmail.com>
2022-09-22 20:00:36 +0700

    upload project vue

---
+++ src/views/HomeView.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="home">
+    <HeaderShayna />
+    <HeroShayna />
+    <WomenShayna />
+    <IstaShayna />
+    <PartnerShayna />
+    <FooterShayna />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue';
+import HeaderShayna from '@/components/HeaderShayna.vue';
+import HeroShayna from '@/components/HeroShayna.vue';
+import WomenShayna from '@/components/WomenShayna.vue';
+import IstaShayna from '../components/IstaShayna.vue';
+import PartnerShayna from '../components/PartnerShayna.vue';
+import FooterShayna from '../components/FooterShayna.vue';
+
+export default {
+  name: 'HomeView',
+  components: {
+    HeaderShayna,
+    HeroShayna,
+    WomenShayna,
+    IstaShayna,
+    PartnerShayna,
+    FooterShayna,
+  },
+};
+</script>
